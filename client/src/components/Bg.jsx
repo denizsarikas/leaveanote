5fbe0981e1510bf34243dfadd54ac630161db1f4
Deniz Sarıkaş <75947870+denizsarikas@users.noreply.github.com>
2023-05-30 14:23:11 +0300

    mobile gif added

--- client/src/components/Bg.jsx
+++ client/src/components/Bg.jsx
@@ -1,10 +1,25 @@
-import videoBg from "../assets/test.mp4"
+import videoBg from "../assets/test.mp4";
+import gifBg from "../assets/runner.gif";
 
-export default function Bg({isMuted}) {
-
-    return (
-        <div className="w-full h-screen">
-            <video className="w-full h-screen object-fill" src={videoBg} autoPlay loop muted={isMuted} />
-        </div>
-    )
+export default function Bg({ isMuted }) {
+  return (
+    <div className="w-full h-screen">
+      <div className="hidden sm:block">
+        <video
+          className="absolute top-0 bottom-0 left-0 right-0 h-screen object-fill w-full"
+          src={videoBg}
+          autoPlay
+          loop
+          muted={isMuted}
+        />
+      </div>
+      <div className="sm:hidden">
+        <img
+          className="absolute top-0 bottom-0 left-0 right-0 w-full h-screen object-fill"
+          src={gifBg}
+          alt="blade-runner"
+        />
+      </div>
+    </div>
+  );
 }
